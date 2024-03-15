4f9e89c540912ba59db27a354efd5403cce0c005
Guido Simon Perre <guido.perre@payclip.com>
2024-03-15 13:55:30 -0300

    Update build.gradle.kts

--- pinpad-sdk/build.gradle.kts
+++ pinpad-sdk/build.gradle.kts
@@ -64,6 +64,17 @@ afterEvaluate {
                 }
             }
         }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/ClipMX/mobile.android.blaze.pinpad.sdk")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
     }
 }
 
